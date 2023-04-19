8007f540966608fdc8f6e4166396b190ceb50ded
Lillie McAlister <lilliemcalister1993@live.com>
2023-04-19 15:15:44 -0700

    Save Edit Changes

--- LAMCRUD/app/src/main/java/com/msla_mac/lamcrud/EditEvent.kt
+++ LAMCRUD/app/src/main/java/com/msla_mac/lamcrud/EditEvent.kt
@@ -6,21 +6,36 @@ import android.view.View
 import android.widget.EditText
 
 class EditEvent : BaseActivity() {
+    lateinit var edtEditName: EditText
+    lateinit var edtEditDescription: EditText
+    lateinit var edtEditStartDate: EditText
+    lateinit var edtEditEndDate: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_event)
-
-        val edtEditName: EditText = findViewById(R.id.edtEditName)
-        val edtEditDescription: EditText = findViewById(R.id.edtEditDescription)
-        val edtEditStartDate: EditText = findViewById(R.id.edtEditStartDate)
-        val edtEditEndDate: EditText = findViewById(R.id.edtEditEndDate)
-
         val event = eventsList[ currentRecord ]
+
+        edtEditName = findViewById(R.id.edtEditName)
+        edtEditDescription = findViewById(R.id.edtEditDescription)
+        edtEditStartDate = findViewById(R.id.edtEditStartDate)
+        edtEditEndDate = findViewById(R.id.edtEditEndDate)
 
         edtEditName.setText(event.name)
         edtEditDescription.setText(event.description)
         edtEditStartDate.setText(event.startDate)
         edtEditEndDate.setText(event.endDate)
+    }
+
+    fun saveChangesOnClick( v : View ){
+        val event = eventsList [ currentRecord ]
+        event.name = edtEditName.text.toString()
+        event.description = edtEditDescription.text.toString()
+        event.startDate = edtEditStartDate.text.toString()
+        event.endDate = edtEditEndDate.text.toString()
+        eventsList[currentRecord] = event
+        val intent = Intent(this, ShowEvent::class.java)
+        startActivity(intent)
     }
 
     fun showAllEventsOnClick( v : View){
