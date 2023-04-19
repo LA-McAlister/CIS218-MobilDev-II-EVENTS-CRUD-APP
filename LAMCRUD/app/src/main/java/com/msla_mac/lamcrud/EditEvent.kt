91a57af4617695c8286f8f5b65566e17acb8c7d7
Lillie McAlister <lilliemcalister1993@live.com>
2023-04-19 02:41:30 -0700

    Switching Activities

--- LAMCRUD/app/src/main/java/com/msla_mac/lamcrud/EditEvent.kt
+++ LAMCRUD/app/src/main/java/com/msla_mac/lamcrud/EditEvent.kt
@@ -1,11 +1,18 @@
 package com.msla_mac.lamcrud
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class EditEvent : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_event)
+    }
+
+    fun showAllEventsOnClick( v : View){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
--- LAMCRUD/app/src/main/java/com/msla_mac/lamcrud/ShowEvent.kt
+++ LAMCRUD/app/src/main/java/com/msla_mac/lamcrud/ShowEvent.kt
@@ -1,11 +1,23 @@
 package com.msla_mac.lamcrud
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class ShowEvent : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_event)
+    }
+
+
+    fun editEventOnClick( v : View){
+        val intent = Intent(this, EditEvent::class.java)
+        startActivity(intent)
+    }
+    fun showAllEventsOnClick( v : View){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
